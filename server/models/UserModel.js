d580bb47c43c742e5c690eacd420570590d19de3
Luke Donovan <lukelamnisos@icloud.com>
2020-06-02 16:20:16 +0100

    began adding validation functionality

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -5,9 +5,10 @@ const User = new Schema(
   {
     name: { type: String, required: true},
     email: { type: String, required: true },
-    username: { type: String, required: true },
+    username: { type: String, required: true, index: { unique: true } },
     password: { type: String, required: true }
-  }
+  },
+  { timestamps: true }
 )
 
 module.exports = mongoose.model('users', User)
