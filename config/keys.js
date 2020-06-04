cbd8020e98d4f9128ca0826a4abb8133616ba40c
Luke Donovan <lukelamnisos@icloud.com>
2020-06-04 15:06:05 +0100

    updated config

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "YOUR_MONGOURI_HERE",
+  mongoURI: "mongodb+srv://lukedonov:jabroni@social-app-gxyx5.mongodb.net/SOCIAL-APP?retryWrites=true&w=majority",
   secretOrKey: "secret"
 };
